95e4415758618b296ca41522c2e1a82a83c4b5ee
ImadIchiri <88448157+ImadIchiri@users.noreply.github.com>
2022-08-28 17:57:18 +0100

    Update main.js

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,5 +1,4 @@
 /*
-
     Hello There I'm Ichiri Imad
     If Ther is anything wrong in this template please feel free to contact me
     
@@ -10,6 +9,9 @@
 
 */
 
+/* 
+    Header Section
+*/
 gsap.from("#main_header", { duration: 1, x: "-100%" });
 
 gsap.from(".list_item", { duration: 0.8, opacity: 0, delay: 1, stagger: 0.2 });
@@ -20,6 +22,28 @@ gsap.fromTo(
   { duration: 0.8, opacity: 0.8, delay: 1.8 }
 );
 
+let isMenuOpened = false;
+const toggleMenu = document.getElementById("toggle_menu");
+toggleMenu.addEventListener("click", () => {
+  if (!isMenuOpened) {
+    gsap.from(".list_item", {
+      duration: 1.2,
+      x: "100%",
+      stagger: 0.2,
+    });
+
+    gsap.fromTo(
+      ".fade_in_btn",
+      { opacity: 0 },
+      { duration: 0.5, opacity: 0.8, delay: 1.2 }
+    );
+  }
+  isMenuOpened = !isMenuOpened;
+});
+
+/* 
+    Home Section
+*/
 gsap.from(".home_text", { duration: 1, x: "-200%", delay: 0.8 });
 
 gsap.from(".home_image", { duration: 1, x: "200%", delay: 0.8 });
